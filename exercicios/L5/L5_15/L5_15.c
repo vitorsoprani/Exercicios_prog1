83ff1f62ed9be781542344d0d644aa85fb3261a9
Vitor S. Passamani <vitor.spassamani@gmail.com>
2024-05-30 18:46:53 -0300

    Resolução da L5_15

---
+++ exercicios/L5/L5_15/L5_15.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int codigo;
+    char sobrenome[21];
+    char nome[21];
+    int nota;
+    int idade;
+} tCandidato;
+
+tCandidato LeCandidato();
+void ImprimeCandidato(tCandidato candidato);
+void OrdenaCrescente(tCandidato * vet, int qtd);
+void TrocaDeLugar(tCandidato *vet, int idx1, int idx2);
+int EhMenorQue(tCandidato cand1, tCandidato cand2);
+int EncontraMenorEntreAeB(tCandidato *vet, int a, int b);
+
+int main() {
+    int qtdCand, i = 0, j;
+    scanf("%d", &qtdCand);
+    tCandidato candidatos[qtdCand];
+    
+    for (i = 0; i < qtdCand; i++) {
+        candidatos[i] = LeCandidato();
+    }
+    
+    OrdenaCrescente(candidatos, qtdCand);
+    
+    for (i = 0; i < qtdCand; i++) {
+        ImprimeCandidato(candidatos[i]);
+    }
+    
+    return 0;
+}
+
+tCandidato LeCandidato() {
+    tCandidato candidato;
+    scanf("%*[^{]");
+    scanf("%*[{ ]");
+    scanf("%d", &candidato.codigo);
+    scanf("%*[, ]");
+    scanf("%[^,],", candidato.sobrenome);
+    scanf("%*[ ]");
+    scanf("%[^,],", candidato.nome);
+    scanf("%d", &candidato.nota);
+    scanf("%*[, ]");
+    scanf("%d", &candidato.idade);
+    scanf("%*[^\n]\n");
+    return candidato;
+}
+
+void ImprimeCandidato(tCandidato candidato) {
+    printf("CAND(%d): %s %s, Nota:%d, Idade:%d\n",
+            candidato.codigo, candidato.nome,
+            candidato.sobrenome, candidato.nota, candidato.idade);
+}
+
+void OrdenaCrescente(tCandidato * vet, int qtd) {
+    int i, idxAux;
+
+    for (i = 0; i < qtd - 1; i++) {
+        idxAux = EncontraMenorEntreAeB(vet, i + 1, qtd - 1);
+        
+        if (EhMenorQue(vet[idxAux], vet[i])) {
+            TrocaDeLugar(vet, i, idxAux);
+        }
+    }
+}
+
+void TrocaDeLugar(tCandidato *vet, int idx1, int idx2) {
+    tCandidato aux = vet[idx1];
+    vet[idx1] = vet[idx2];
+    vet[idx2] = aux;
+}
+
+int EhMenorQue(tCandidato cand1, tCandidato cand2) {
+    if (!strcmp(cand1.nome, cand2.nome)) {
+        return strcmp(cand1.sobrenome, cand2.sobrenome) < 0;
+    } else {
+        return strcmp(cand1.nome, cand2.nome) < 0;
+    }
+}
+
+int EncontraMenorEntreAeB(tCandidato *vet, int a, int b) {
+    int idxMenor = a;
+    int i;
+    
+    for (i = a + 1; i <= b; i++) {
+        if (EhMenorQue(vet[i], vet[idxMenor])) {
+            idxMenor = i;
+        }
+    }
+    
+    return idxMenor;
+}
--- exercicios/L5/L5_15/fazdiff.sh
+++ exercicios/L5/L5_15/fazdiff.sh
@@ -4,8 +4,8 @@ gcc L5_15.c -o teste
 ./teste <input/test_2 >output/out_2
 
 echo "TESTE 1:"
-diff output/test_1 output/out_1
+diff output/test_1 output/out_1 --color -u
 echo "TESTE 2:"
-diff output/test_2 output/out_2
+diff output/test_2 output/out_2 --color -u
 
 rm teste
