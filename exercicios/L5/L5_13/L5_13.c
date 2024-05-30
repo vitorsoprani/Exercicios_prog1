dd6116b311717f6186037b0a43e3d34d46bbcf32
Vitor S. Passamani <vitor.spassamani@gmail.com>
2024-05-30 16:10:25 -0300

    Resolução da L5_13

---
+++ exercicios/L5/L5_13/L5_13.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ComparaString(char * str1, char * str2);
+
+int main() {
+    char str1[1000];
+    char str2[1000];
+
+    while (scanf("%s", &str1) == 1 && scanf("%s", &str2) == 1) {
+        if (ComparaString(str1, str2))
+            printf("IGUAL\n");
+        else
+            printf("DIFERENTE\n");
+    }
+
+    return 0;
+}
+
+int ComparaString(char * str1, char * str2) {
+    int iguais = 1;
+    int i = 0;
+
+    while (str1[i] && str2[i]) {
+        if (str1[i] != str2[i]) return 0;
+        i++;
+    }
+
+    if (str1[i] || str2[i]) return 0;
+    return 1;
+}
