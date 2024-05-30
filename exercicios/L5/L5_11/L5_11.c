cc23f17a55f9a65121e93df618df73e8e4797930
Vitor S. Passamani <vitor.spassamani@gmail.com>
2024-05-30 15:37:00 -0300

    resolução correta da L5_11

--- exercicios/L5/L5_11/L5_11.c
+++ exercicios/L5/L5_11/L5_11.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void LeVetor(int vet[], int qtd);
-void OrdenaCrescente(int vet[], int qtd, char vetSaida[]);
+void OrdenaCrescente(int vet[], int qtd);
 int IdxDoMenorEntreAeB(int vet[], int a, int b);
 int EhMaiorQue(int a, int b);
 void TrocaDeLugarInt(int vet[], int idxA, int idxB);
@@ -10,53 +10,47 @@ void ImprimeVetorInt(int vet[], int qtd);
 void ImprimeVetorChar(char vet[], int qtd);
 void InicializaVetSaida(char vet[], int qtd1, int qtd2);
 void UneVetores(int vet1[], int qtd1, int vet2[], int qtd2, int vetResult[]);
+void ImprimeResultado(int vet1[], int qtd1, int vet2[], int qtd2);
 
 int main() {
     int qtd1, qtd2;
-    
+
     scanf("%d", &qtd1);
     int vet1[qtd1];
     LeVetor(vet1, qtd1);
+    OrdenaCrescente(vet1, qtd1);
     
     scanf("%d", &qtd2);
     int vet2[qtd2];
     LeVetor(vet2, qtd2);
+    OrdenaCrescente(vet2, qtd2);
     
-    int vetResult [qtd1 + qtd2];
-    UneVetores(vet1, qtd1, vet2, qtd2, vetResult);
+    ImprimeResultado(vet1, qtd1, vet2, qtd2);
     
-    char vetSaida[qtd1 + qtd2];
-    InicializaVetSaida(vetSaida, qtd1, qtd2);
-    
-    OrdenaCrescente(vetResult, qtd1 + qtd2, vetSaida);
-    
-    ImprimeVetorChar(vetSaida, qtd1 + qtd2);
-
     return 0;
 }
 
 void LeVetor(int vet[], int qtd) {
     //Essa função lê os dados a serem armazenados em um vetor índice a índice.
     int i;
-    
+
     //percorre todos os índeces do vetor
     for (i = 0; i < qtd; i++) {
         scanf("%d", &vet[i]);
     }
 }
 
-void OrdenaCrescente(int vet[], int qtd, char vetSaida[]) {
+void OrdenaCrescente(int vet[], int qtd) {
     //Essa função ordena um vetor em ordem crescente.
     int i, idxAux;
-    
+
     //loop que percorre todos os índices do vetor exceto pelo ultimo.
     //(pelo funcionamento do algoritmo, o ultimo índice não precisa ser verificado)
     for (i = 0; i < qtd - 1; i++) {
         idxAux = IdxDoMenorEntreAeB(vet, i + 1, qtd - 1);
-        
+
         if (EhMaiorQue(vet[i], vet[idxAux])) {
             TrocaDeLugarInt(vet, i, idxAux);
-            TrocaDeLugarChar(vetSaida, i, idxAux);
         }
     }
 }
@@ -67,7 +61,7 @@ int IdxDoMenorEntreAeB(int vet[], int a, int b) {
     int idxDoMenor = a, i;
 
     for (i = a + 1; i <= b; i++) {
-        if (EhMaiorQue(vet[a], vet[i])) {
+        if (EhMaiorQue(vet[idxDoMenor], vet[i])) {
             idxDoMenor = i;
         }
     }
@@ -127,5 +121,31 @@ void UneVetores(int vet1[], int qtd1, int vet2[], int qtd2, int vetResult[]) {
     }
     for (i = 0; i < qtd2; i++) {
         vetResult[qtd1 + i] = vet2[i];
+    }
+}
+
+void ImprimeResultado(int vet1[], int qtd1, int vet2[], int qtd2) {
+    int i = 0, j = 0;
+
+    while (i < qtd1 && j < qtd2) {
+        //capara os vetores valor a valor printando A ou B
+        if (vet1 [i] <= vet2[j]) {
+            printf("A");
+            i++;
+        } else {
+            printf("B");
+            j++;
+        }
+    }
+
+    //termina de imprimir o resultado para os valores remanescentes (caso existam)
+    if (i < qtd1) {
+        for (i; i < qtd1; i++) {
+            printf("A");
+        }
+    } else if (j < qtd2) {
+        for (j; j < qtd2; j++) {
+            printf("B");
+        }
     }
 }
