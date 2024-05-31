50dbbe80f5671a3f8cade3c9d07a95060cf61052
Vitor S. Passamani <vitor.spassamani@gmail.com>
2024-05-30 21:07:00 -0300

    resolução da L5_17 com gambiarra

---
+++ exercicios/L5/L5_17/L5_17.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define VALORMAX 500
+
+int LeTexto(int l, int c, char texto[l][c]);
+void ImprimeTextoInvertido(int qtdPal, int l, int c, char texto[l][c]);
+
+int main() {
+    char texto [VALORMAX] [VALORMAX];
+    int qtdPalavras;
+
+    qtdPalavras = LeTexto(VALORMAX, VALORMAX, texto);
+
+    ImprimeTextoInvertido(qtdPalavras, VALORMAX, VALORMAX, texto);
+    
+    return 0;
+}
+
+int LeTexto(int linhas, int colunas, char texto[linhas][colunas]) {
+    int i = 0;
+    while (scanf("%[a-zA-Z]", texto[i]) == 1) {
+        i++;
+        scanf("%*[^a-zA-Z]");
+    }
+    return i;
+}
+
+void ImprimeTextoInvertido(int qtdPal, int l, int c, char texto[l][c]) {
+    int i;
+    
+    printf(" ");
+    for (i = qtdPal - 1; i >= 0; i--) {
+        printf("%s", texto[i]);
+        printf(" ");
+    }
+}
