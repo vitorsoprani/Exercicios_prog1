fc1c007e3836575db9fc7a099a7b20578c185a29
Vitor S. Passamani <vitor.spassamani@gmail.com>
2024-06-04 11:03:22 -0300

    Resolução do exercicio 5 da monitoria do pet

--- pet/exercicios/ex_5/ex_5.c
+++ pet/exercicios/ex_5/ex_5.c
@@ -15,9 +15,7 @@ typedef struct {
 } Pokemon;
 
 typedef struct {
-    /*
-    acopla o pokemon de diogo, do adversario e informacoes que podem ser uteis, como qtdDeAdversarios
-    */
+    //acopla o pokemon de diogo, do adversario e informacoes que podem ser uteis, como qtdDeAdversarios
     Pokemon pokemonDiogo;
     int qtdDeAdversarios;
     int batalhaAtual;
@@ -25,37 +23,37 @@ typedef struct {
 
 } Ginasio;
 
-float calculaVantagem(Pokemon atacante, Pokemon defensor);//feito
+float calculaVantagem(Pokemon atacante, Pokemon defensor);
 /*
 Retorna o valor de vantagem que é 0.5(desvantagem), 1.5(vantagem) ou 1.0(duelo equilibrado)
 */
 
-int calculaDano(Pokemon atacante, Pokemon defensor);//feito
+int calculaDano(Pokemon atacante, Pokemon defensor);
 /*
 Dentro da funcao, recebe o valor da vantagem e utiliza a formula dada para calcular a quantidade de dano na vida do oponente
 obs: utilizar floor para arrendondar com mais precisao
 */
-int estaVivo(Pokemon p); //feito
+int estaVivo(Pokemon p);
 /*
 Verifica se o pokemon esta vivo
 retorna 1 caso verdadeiro
 retorna 0 caso falso
 */
 
-int menorNivel(Pokemon atacante, Pokemon defensor);//feito
+int menorNivel(Pokemon atacante, Pokemon defensor);
 /*
 Verifica se o atacante tem menor nivel que o defensor
 retorna 1 caso verdadeiro
 retorna 0 caso falso
 */
 
-Pokemon subirDeNivel(Pokemon p);//feito
+Pokemon subirDeNivel(Pokemon p);
 /*
 Sobe o nivel do pokemon e atualiza seus status
 retorna p atualizado
 obs: utilizar floor para arrendondar com mais precisao
 */
-Ginasio batalhar(Pokemon atacante, Pokemon defensor, Ginasio ginasio);//feito
+Ginasio batalhar(Pokemon atacante, Pokemon defensor, Ginasio ginasio);
 /*
 funcao que recebe o pokemon atacante, defensor e o ginasio.
 Aqui que a batalha eh efetuada
@@ -64,24 +62,24 @@ no final, atribua defensor e atacante ao ginasio
 retorna ginasio atualizado
 obs1: o pokemon de maior velocidade ataca primeiro
 */
-Pokemon lePokemon(); //feito
+Pokemon lePokemon();
 /*
 Declara pokemon
 Efetua a leitura do pokemon
 retorna pokemon
 */
-void imprimePokemon(Pokemon p); //feito
+void imprimePokemon(Pokemon p);
 /*
 Imprime todas as informacoes do pokemon p
 */
-Ginasio inicializaGinasio(); //feito
+Ginasio inicializaGinasio();
 /*
 Declara ginasio
 Faz a leitura do pokemon de diogo, numDeAdversarios e inicializa as outras variaveis
 retorna ginasio
 */
 
-Ginasio RealizaBatalha(Ginasio ginasio); //feito
+Ginasio RealizaBatalha(Ginasio ginasio);
 /*
 Recebe o ginasio inicializado e efetua as batalhas ate que diogo venca ou perca
 retorna ginasio atualizado
@@ -92,13 +90,13 @@ Imprime todas as informacoes da batalhas, especificadas no enunciado e casos de 
 */
 
 int main() {
-    Ginasio ginasio = inicializaGinasio(); //ok
+    Ginasio ginasio = inicializaGinasio();
     ginasio = RealizaBatalha(ginasio);
-    FinalizaBatalha(ginasio);//ok
+    FinalizaBatalha(ginasio);
     return 0;
 }
 
-float calculaVantagem(Pokemon atacante, Pokemon defensor) {
+float calculaVantagem(Pokemon atacante, Pokemon defensor) { //deve dar pra otimizar, mas sem tempo irmao
     switch (atacante.tipo) {
     case 'W':
         if (defensor.tipo == 'F' || defensor.tipo == 'R' || defensor.tipo == 'G') {
@@ -160,10 +158,11 @@ float calculaVantagem(Pokemon atacante, Pokemon defensor) {
 int calculaDano(Pokemon atacante, Pokemon defensor) {
     int dano;
     int nivel = atacante.nivel;
+    int atk = atacante.ataque;
     int def = defensor.defesa;
-    int adv = calculaVantagem(atacante, defensor);
+    float adv = calculaVantagem(atacante, defensor);
 
-    dano = floor((((0.4 * nivel + 2) * (def)) + 2) * adv);
+    dano = floor((((0.4 * nivel + 2) * (atk / def)) + 2) * adv);
 
     return dano;
 }
@@ -180,7 +179,7 @@ Pokemon subirDeNivel(Pokemon p) {
     p.ataque += floor(p.ataque * 0.1);
     p.defesa += floor(p.defesa * 0.1);
     p.velocidade += floor(p.velocidade * 0.1);
-    p.vidaMaxima += floor(p.vidaAtual * 0.1);
+    p.vidaMaxima += floor(p.vidaMaxima * 0.1);
     p.vidaAtual = p.vidaMaxima;
     p.nivel++;
 
@@ -240,7 +239,7 @@ void imprimePokemon(Pokemon p) {
     printf("Nome: %c\n", p.nome);
     printf("Tipo: %c\n", p.tipo);
     printf("Nivel: %d\n", p.nivel);
-    printf("VidaMax: %d\n", p.vidaMaxima);
+    printf("VidaMax: %d \n", p.vidaMaxima);
     printf("VidaAtual: %d\n", p.vidaAtual);
     printf("Ataque: %d\n", p.ataque);
     printf("Defesa: %d\n", p.defesa);
@@ -264,10 +263,8 @@ Ginasio inicializaGinasio() {
 }
 
 Ginasio RealizaBatalha(Ginasio ginasio) {
-    int i = 0;
-    while (estaVivo(ginasio.pokemonDiogo) && i < ginasio.qtdDeAdversarios) {
-        ginasio = batalhar(ginasio.pokemonDiogo, ginasio.adversarios[i], ginasio);
-        i++;
+    while (estaVivo(ginasio.pokemonDiogo) && ginasio.batalhaAtual < ginasio.qtdDeAdversarios) {
+        ginasio = batalhar(ginasio.pokemonDiogo, ginasio.adversarios[ginasio.batalhaAtual], ginasio);
     }
     return ginasio;
 }
@@ -278,8 +275,8 @@ void FinalizaBatalha(Ginasio ginasio) {
         printf("Pokemon de Diogo:\n");
         imprimePokemon(ginasio.pokemonDiogo);
     } else {
-        printf("Diogo perdeu na %d batalha\n", ginasio.batalhaAtual);
+        printf("Diogo perdeu na %d batalha!\n", ginasio.batalhaAtual);
         printf("Ultimo Pokemon adversario:\n");
-        imprimePokemon(ginasio.adversarios[ginasio.batalhaAtual]);
+        imprimePokemon(ginasio.adversarios[ginasio.batalhaAtual - 1]);
     }
 }
